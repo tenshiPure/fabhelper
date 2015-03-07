61153580ddacf455eee7b5a4632f2349ea08d0a2
tenshiPure <user.ryo@gmail.com>
2015-03-07 23:34:22 +0900

    master : create file.conf

--- fabfile/fabhelper/file.py
+++ fabfile/fabhelper/file.py
@@ -10,3 +10,24 @@ def sed(path, src, dst, e = False):
 
 def __escape(string):
 	return string.replace('/', '\/').replace('"', '\"')
+
+
+def conf(func):
+	def _conf(path):
+		__backup(path)
+		func(path)
+		__diff(path)
+
+	return _conf
+
+
+def __backup(path):
+	if 'No such file or directory' in sudo('ls %s.origin; true' % path):
+		sudo('cp -p %s %s.origin' % (path, path))
+		ok('echo create backup : %s.origin' % path)
+	else:
+		ok('echo %s.origin is already exists' % path)
+
+
+def __diff(path):
+	ok('diff %s %s.origin; true' % (path, path))
--- fabfile/file.py
+++ fabfile/file.py
@@ -11,3 +11,14 @@ def sed():
 @task
 def sed_e():
 	file.sed('/tmp/sample.txt', '^hoge', 'aaaaa', e = True)
+
+
+@task
+def conf():
+	__sed_with_conf('/tmp/sample.txt')
+
+
+@file.conf
+def __sed_with_conf(path):
+	file.sed(path, 'hoge', 'aaaaa')
+	file.sed(path, 'fuga', 'bbbbb')
