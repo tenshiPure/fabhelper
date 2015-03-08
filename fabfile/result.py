153afef9c7186f260970c1425bc261ab744bf7af
tenshiPure <user.ryo@gmail.com>
2015-03-08 23:34:01 +0900

    master : modify result selectable color

--- fabfile/result.py
+++ fabfile/result.py
@@ -4,20 +4,35 @@ from fabhelper import result
 
 @task
 def done():
-	result.done('uname -a')
-	result.done('echo some string')
+	result.done("echo 'done    : default color'")
 
 
 @task
 def error():
-	result.error('uname -a')
-	result.error('echo some string')
+	result.error("echo 'error   : default color'")
 
 
 @task
 def already():
-	result.already('uname -a')
-	result.already('echo some string')
+	result.already("echo 'already : default color'")
+
+
+@task
+def done_change_color_specify_with_argument():
+	result.done("echo 'done    : specify with argument'", color = result.blue)
+
+
+@task
+def done_change_color_specify_setter():
+	result.set_color_done(result.magenta)
+	result.done("echo 'done    : specify with setter'")
+	result.reset_color_all()
+
+	result.done("echo 'done    : default color'")
+
+@task
+def bold():
+	result.done("echo 'done    : bold'", color = result.yellow, bold = True)
 
 
 @task
@@ -25,3 +40,9 @@ def all():
 	done()
 	error()
 	already()
+
+	done_change_color_specify_with_argument()
+
+	done_change_color_specify_setter()
+
+	bold()
